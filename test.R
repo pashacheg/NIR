258753b36e243c6c9cc4f95af78becc5ba1cd595
pavelin135 <116956485+pavelin135@users.noreply.github.com>
2023-04-24 23:12:33 +0300

    Update test.R

--- test.R
+++ test.R
@@ -12,3 +12,24 @@ summary(model2)
 
 model3 = lm(privileges ~ complaints + raises, attitude)
 summary(model3)
+
+
+
+newmodel = lm(rating ~ complaints, attitude)
+summary(newmodel)
+
+newmodel1 = lm(rating ~ complaints + I(raises^2), attitude)
+summary(newmodel1)
+
+newmodel2 = lm(rating ~ complaints + I(privileges^2), attitude)
+summary(newmodel2)
+
+newmodel3 = lm(rating ~ complaints + I(privileges * raises), attitude)
+summary(newmodel3)
+
+newmodel4 = lm(rating ~ complaints + log(raises), attitude)
+summary(newmodel4)
+
+newmodel5 = lm(rating ~ complaints + log(privileges), attitude)
+summary(newmodel5)
+
